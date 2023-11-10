28f5ea607ad339560556ee6d0506bfe4b31def9a
Marius Buleandra <mbuleandra@gmail.com>
2023-11-10 15:36:03 -0800

    store db

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import random
 import string
 
 import boto3
+import databases
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pipeline import temporal
-from pipeline.lib import Job, JobStatus, is_status_final
+from lib.types import Job, JobIn, JobStatus, is_status_final, LanguageCode
+import sqlalchemy
 
 
 load_dotenv()
 
-app = FastAPI()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 TEMPORAL_URL = os.getenv("TEMPORAL_URL")
 TEMPORAL_NAMESPACE = os.getenv("TEMPORAL_NAMESPACE") or "default"
@@ -21,6 +23,38 @@ AWS_S3_ENDPOINT_URL = os.getenv("AWS_S3_ENDPOINT_URL")
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_S3_BUCKET = os.getenv("AWS_S3_BUCKET")
+
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+jobs_table = sqlalchemy.Table(
+    "jobs",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.String, primary_key=True),
+    sqlalchemy.Column("input_url", sqlalchemy.String, nullable=False, server_default=""),
+    sqlalchemy.Column("output_url", sqlalchemy.String, nullable=False, server_default=""),
+    sqlalchemy.Column("target_language", sqlalchemy.String, nullable=False, server_default=LanguageCode.en),
+    sqlalchemy.Column("status", sqlalchemy.String, nullable=False, server_default=JobStatus.created),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, nullable=False, server_default=sqlalchemy.sql.func.now()),
+)
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL
+)
+metadata.create_all(engine)
+
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 
 s3 = boto3.client('s3',
     endpoint_url = AWS_S3_ENDPOINT_URL,
@@ -38,15 +72,23 @@ app.add_middleware(
 )
 
 
-JOBS: dict[str, Job] = dict()
-
-
 @app.options("/jobs")
 @app.post("/jobs")
-async def create_job(job: Job):
-    job.id = random_id()
-    job.status = JobStatus.created
-    print("creating job", job)
+async def create_job(job_in: JobIn) -> Job:
+    job = Job(
+        id=random_id(),
+        status=JobStatus.created,
+        input_url=job_in.input_url,
+        target_language=job_in.target_language,
+    )
+
+    query = jobs_table.insert().values(
+        id=job.id,
+        input_url=job.input_url,
+        target_language=job.target_language,
+        status=job.status,
+    )
+    await database.execute(query)
 
     temporal_client = await temporal.get_client(
         TEMPORAL_URL, TEMPORAL_NAMESPACE)
@@ -56,24 +98,34 @@ async def create_job(job: Job):
         job.id,
         job.input_url, 
         job.target_language)
-    
-    JOBS[job.id] = job
+
     return job
 
 
 @app.get("/jobs/{job_id}")
 async def get_job(job_id: str) -> Job:
-    job = JOBS.get(job_id)
-    if not job:
-        return Job(id=job_id, status=JobStatus.failed)
+    query = jobs_table.select().where(jobs_table.c.id == job_id)
+    row = await database.fetch_one(query)
+    if not row:
+        raise HTTPException(status_code=404, detail="Job not found")
+    job = Job(**row)
+
     if is_status_final(job.status):
         return job
+
     temporal_client = await temporal.get_client(
         TEMPORAL_URL, TEMPORAL_NAMESPACE)
     status, output = await temporal.describe_workflow(temporal_client, job_id)
+    
     job.status = status
     job.output_url = get_presigned_url(output) if output else ""
-    JOBS[job_id] = job
+
+    query = jobs_table.update().where(jobs_table.c.id == job_id).values(
+        status=job.status,
+        output_url=job.output_url,
+    )
+    await database.execute(query)
+
     return job
 
 def random_id() -> str:
